56e6a58a9b92ac5869a450cbcd7d778ef9868ee6
Jeremias Ryttäri <jeremias.ryttari@smartly.io>
2024-12-11 09:34:53 +0200

    day 11 - 1, refactored

--- day_11/main.go
+++ day_11/main.go
@@ -1,65 +1,22 @@
 package main
 
 import (
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type Stone struct {
-	val string
-}
+func blink(i int) (int, int) {
+	length := int(math.Log10(float64(i)) + 1)
+	if i == 0 {
+		return 1, -1
+	} else if length&1 == 0 {
+		split := int(math.Pow10(length - length/2))
+		return i / split, i % split
 
-func (s *Stone) blink() []Stone {
-	r := []Stone{}
-	if s.isZero() {
-		r = append(r, Stone{val: "1"})
-	} else if s.hasEvenNumberOfDigits() {
-		r = append(r, s.split()...)
-	} else {
-		i, _ := strconv.Atoi(s.val)
-		v := strconv.Itoa(i * 2024)
-		r = append(r, Stone{val: v})
 	}
-
-	return r
-}
-
-func (s *Stone) isZero() bool {
-	return s.val == "0"
-}
-
-func (s *Stone) hasEvenNumberOfDigits() bool {
-	return len(s.val)&1 == 0
-}
-
-func (s *Stone) split() []Stone {
-	arr := strings.Split(s.val, "")
-
-	half := len(arr) / 2
-
-	first, second := strings.Join(arr[0:half], ""), strings.Join(arr[half:], "")
-
-	if allZerosAndLongerThanOne(second) {
-		second = second[len(second)-2 : len(second)-1]
-	} else if len(second) > 1 {
-		second = strings.TrimPrefix(second, "0")
-	}
-
-	return []Stone{{val: first}, {val: second}}
-}
-
-func allZerosAndLongerThanOne(s string) bool {
-	if len(s) == 1 {
-		return false
-	}
-	for _, ch := range s {
-		if ch != '0' {
-			return false
-		}
-	}
-
-	return true
+	return i * 2024, -1
 }
 
 func main() {
@@ -74,18 +31,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	stones := []Stone{}
+	stones := []int{}
 	input := string(b)
 	for _, s := range strings.Split(strings.TrimSpace(input), " ") {
-		stones = append(stones, Stone{val: s})
+		i, _ := strconv.Atoi(s)
+		stones = append(stones, i)
 	}
 
 	blinks := 25
 
 	for blinks > 0 {
-		new := []Stone{}
+		new := []int{}
 		for _, stone := range stones {
-			new = append(new, stone.blink()...)
+			first, second := blink(stone)
+			new = append(new, first)
+			if second >= 0 {
+				new = append(new, second)
+			}
 		}
 		stones = new
 		blinks--
